c8f7bb091afe9776e317e47009bbf440c9796887
YesPro34 <echamkh786@gmail.com>
2023-04-14 16:02:05 +0000

    show products to clients

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,24 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
 
 
     public function index(){
-        return view('home.userpage');
+        $products = Product::paginate(9);
+        return view('home.userpage',compact('products'));
     }
 
     
     public  function redirect(){
         // check for user type
         $userType = Auth::user()->usertype;
-
         if($userType == '1'){
             return view('admin.home');
         }else{
-            return view('home.userpage');
+            $products = Product::paginate(9);
+            return view('home.userpage',compact('products'));
         }
     }
 
